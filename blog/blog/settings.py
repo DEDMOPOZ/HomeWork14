250ad1d757fcd28cbb1ae31563dae7da7d5a7e70
Viacheslav <71900484+DEDMOPOZ@users.noreply.github.com>
2021-05-09 22:37:33 +0300

    Update settings.py

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -144,8 +144,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 #Gmail
-EMAIL_HOST_USER = 'dedmopoz.temp@gmail.com'
-EMAIL_HOST_PASSWORD = 'irakez123'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
